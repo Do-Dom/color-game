f1ac0b7a11a5f05eefe0adb5fced3b2906a532fd
도돔 <yunjeong.syj@gmail.com>
2019-12-29 16:20:18 +0900

    edit color game

--- colorgame.js
+++ colorgame.js
@@ -1,10 +1,16 @@
 const container = document.querySelector("#container");
 const squares = container.querySelectorAll(".square");
 const colorInfoText = document.querySelector("#colorInfo");
+const topArea = document.querySelector("h1");
 const navigation = document.querySelector("#navigation")
- 
+const resetBtn = navigation.querySelector(".reset");
+const modeBtns = navigation.querySelectorAll(".mode");
+
+let squareNum;
 let targetIndex;
+let targetColor;
 let colorList = new Array();
+let gameover = false;
 
 
 function onMouseOver(event)
@@ -20,28 +26,102 @@ function onMouseOut(event)
     if(event.target.classList.contains("navBtn")==false)
         return; 
 
+    if(event.target.classList.contains("selected"))
+        return; 
+
     event.target.classList.remove("btnMouseOver");
+}
+
+function setMode(isEasy)
+{
+    if(isEasy)
+    {
+        squareNum = 6;
+        for(i=6; i<squares.length; i++)
+        {
+            squares[i].classList.add("notUse");
+        }
+
+        modeBtns[0].classList.add("selected");
+        modeBtns[0].classList.add("btnMouseOver");
+
+        modeBtns[1].classList.remove("selected");
+        modeBtns[1].classList.remove("btnMouseOver");
+
+    }
+    else
+    {
+        squareNum = 9;
+        for(i=6; i<squares.length; i++)
+        {
+            squares[i].classList.remove("notUse");
+        }
+
+        
+        modeBtns[1].classList.add("selected");
+        modeBtns[1].classList.add("btnMouseOver");
+
+        modeBtns[0].classList.remove("selected");
+        modeBtns[0].classList.remove("btnMouseOver");
+    }
+
+    resetColorGame();
+}
+
+function onClickModeBtn(event)
+{
+    modeBtns.forEach(btn => {
+       
+        if(btn === event.target)
+        {
+            btn.classList.add("selected");
+            btn.classList.add("btnMouseOver");
+        }
+        else
+        {
+            btn.classList.remove("btnMouseOver");
+            btn.classList.remove("selected");
+        }  
+    });
+
+    if(event.target.textContent === "easy")
+    {
+        setMode(true);
+    }
+    else
+    {
+        setMode(false);
+    }
+
+    resetColorGame();
+}
+
+function onClickResetBtn(event)
+{
+    resetColorGame();
 }
 
 function OnClickEventHandler(event)
 {
+    if(gameover === true)
+        return;
+
     if(event.target.classList.contains("square") === false)
         return;
        
 
     if(event.target.classList.contains(targetIndex))
     {
-        console.log("Correct!");
-        setSquareColor();
+        gameover = true;
+        setCorrectColor(event.target.style.backgroundColor);
     }
     else
     {
-        console.log("NOOOOOOOOOOOO");
         event.target.classList.add("hidden");
     }
 }
 
-function getRandomRGB()
+function generateRandomColor()
 {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
@@ -50,12 +130,26 @@ function getRandomRGB()
 
 }
 
-function setSquareColor()
+function setCorrectColor(color)
 {
-    targetIndex = Math.floor( Math.random() * squares.length);
+    topArea.style.backgroundColor = color;
+    squares.forEach((square)=>
+    {
+        square.classList.remove("hidden");
+        square.style.backgroundColor = color;
+
+    });
+}
+
+function resetColorGame()
+{
+    gameover = false;
+
+    topArea.style.backgroundColor = "#4682b4";
+    targetIndex = Math.floor( Math.random() * squareNum);
 
     squares.forEach((currentValue, currentIndex) => {
-        const color = getRandomRGB()
+        const color = generateRandomColor()
         colorList.push(color);
         currentValue.style.backgroundColor = color;
 
@@ -76,8 +170,13 @@ function init()
     container.addEventListener("click", OnClickEventHandler);
     navigation.addEventListener("mouseover", onMouseOver);
     navigation.addEventListener("mouseout", onMouseOut);
+    resetBtn.addEventListener("click", onClickResetBtn);
 
-    setSquareColor();
+    modeBtns.forEach(element => {
+        element.addEventListener("click", onClickModeBtn);
+    });
+
+    setMode(true);
 }
 
 init();
